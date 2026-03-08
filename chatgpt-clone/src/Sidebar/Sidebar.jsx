ce3f926ff4dee712c38d1d5f68086aefe85e22dd
ShamitGupta <e1459301@u.nus.edu>
2026-03-08 19:47:48 +0800

    Made sidebar better

--- chatgpt-clone/src/Sidebar/Sidebar.jsx
+++ chatgpt-clone/src/Sidebar/Sidebar.jsx
@@ -1,23 +1,24 @@
 import styles from './Sidebar.module.css'
 import logo from '../assets/shamo-logo-for-chatbot.png'
+
 function Sidebar() {
     return (
-        <>
-            <div className = {styles.container}>
-                <div className = {styles.chatOptions}>
-                    <button className = {styles.chatButtons}>New Chat</button>
-                    <button className = {styles.chatButtons}>About Us</button>
-                    <button className = {styles.chatButtons}>Report an Issue</button>
-                </div>
-
-                <div className = {styles.profileContainer}>
-                    <div className = {styles.profile}>
-                        <img src = {logo} className = {styles.logo}></img>
-                        <p>Shamit Gupta</p>
-                    </div>
+        <aside className={styles.container}>
+            <div className={styles.upperSection}>
+                <div className={styles.chatOptions}>
+                    <button className={styles.chatButtons}>New Chat</button>
+                    <button className={styles.chatButtons}>About Us</button>
+                    <button className={styles.chatButtons}>Report an Issue</button>
                 </div>
             </div>
-        </>
+
+            <div className={styles.profileContainer}>
+                <div className={styles.profile}>
+                    <img src={logo} alt="User Logo" className={styles.logo} />
+                    <p className={styles.userName}>Shamit Gupta</p>
+                </div>
+            </div>
+        </aside>
     );
 }
 
